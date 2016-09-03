045a837558cbf55f254c4bb94136e38967dc127a
Tanmai Gopal <tanmaig@34cross.in>
2016-09-04 01:57:25 +0530

    Changes to a simpler scope.

--- app/src/components/Login/Login.js
+++ app/src/components/Login/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
       <div className={styles.container}>
         <Helmet title="Login | IMAD console" />
         <div className={styles.centerBox}>
-          <a className="btn btn-lg btn-primary" href="https://github.com/login/oauth/authorize?client_id=bcaac3b822c108523d5b&scope=user%20repo&redirect_uri=http://api.imad-stg.hasura-app.io/github/authenticate" role="button">
+          <a className="btn btn-lg btn-primary" href="https://github.com/login/oauth/authorize?client_id=bcaac3b822c108523d5b&scope=user%20public_repo&redirect_uri=http://api.imad-stg.hasura-app.io/github/authenticate" role="button">
             <span className={styles.icon}><i className="fa fa-github" /></span>
             Sign in with github
           </a>
