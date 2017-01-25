ad4a9b778e98557984b6780278a37c83078fb07f
Shahidh K Muhammed <shahidh@hasura.io>
2017-01-25 15:54:19 +0530

    Revert to old state

--- app/runserver.sh
+++ app/runserver.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 cd /app
-if [ "$API_ENV" = "prod" ]; then
-    npm run start-prod
-elif [ "$API_ENV" = "dev" ];then    
-    npm run start-dev
-else
-    echo "API_ENV not set\n"
-fi
+npm run start-prod
